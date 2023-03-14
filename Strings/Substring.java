afb1dd3239ba8b9ba7a329799d6238f100239256
Neel_Lohith <95930550+NAKALAMBA@users.noreply.github.com>
2023-03-14 16:01:43 +0530

    Update Substring.java

--- Strings/Substring.java
+++ Strings/Substring.java
@@ -1,5 +1,15 @@
 package Codes.Strings;
 
 public class Substring {
-    
+    public static String subString(String str , int si , int ei){ // Starting index and ending index in a string 
+        String substr = "";
+        for( int i = si ; i < ei ; i++ ){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
+    public static void main(String args[]){
+        String str = "Neel lohith";
+        System.out.println(subString(str, 0, 2));
+    }
 }
